52bde9893ab0eb033326f27cf64e78bb08a7a41c
mikutakeuchi <mikutakeuchi@ucl.meijo-u.ac.jp>
2018-04-26 14:50:13 +0900

    「hello world!」と表示するプログラムに変更

--- test.java
+++ test.java
@@ -1,22 +1,6 @@
-public class Task1A {
+public class test {
 	public static void main(String[] args) {
-		int n = 1000;
-		int[] a = new int[1024];//‚Ó‚é‚¢
 		
-		//‰Šú‰»
-		for (int i = 2; i < n; i++){ 
-			a[i] = 1;
-		}
-		
-		//‘f”ˆÈŠO‚Ì”‚ğ‚Í‚¶‚¢‚Ä‚¢‚­
-		for (int i = 2; i < n; i++){
-			if (a[i] == 1){
-				for(int j = i * 2; j < n; j += i){
-					a[j] = 0;
-				}
-				System.out.print(i + " ");
-			}
-		}
-		
+				System.out.print("hello world!");
 	}
 }
